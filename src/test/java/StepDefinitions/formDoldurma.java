8d5782088fbce326702ad4dda83a1c1c58bf78d6
Husedemir77 <husedemirde@gmail.com>
2020-10-10 14:24:51 +0200

    1010

---
+++ src/test/java/StepDefinitions/formDoldurma.java
@@ -0,0 +1,52 @@
+package StepDefinitions;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class formDoldurma {
+
+    @Given("^Navigate to Website$")
+    public void navigate_to_Website() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^Enter Ad and Soyad$")
+    public void enter_Ad_and_Soyad() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^Enter E-Mail$")
+    public void enter_E_Mail() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^Enter Alan Kodu and Telefon Numarasi$")
+    public void enter_Alan_Kodu_and_Telefon_Numarasi() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^Choose one of the hangisi options$")
+    public void choose_one_of_the_hangisi_options() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^Click gonder button$")
+    public void click_gonder_button() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^Verify the success message$")
+    public void verify_the_success_message() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+}
